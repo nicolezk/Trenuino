6aa3842c6a5606b355e52ad48e75454944f1ba85
nicolezk1 <nicolezk1@gmail.com>
2017-01-18 17:58:52 -0200

    Acrescentado motor de passos

--- README.md
+++ README.md
@@ -5,9 +5,28 @@ Trabalho final da disciplina de Laboratório de Microcontroladores e Aplicaçõe
 O projeto a ser desenvolvido pelo grupo consiste em uma aplicação de um **controlador capaz de efetuar o mapeamento da planta de um cômodo residencial** (ou qualquer outro ambiente totalmente delimitado). 
 
 ### Material
- - microcontrolador Arduíno
- - sensor ultrassônico de distância
- - motor de passo. 
+ - microcontrolador Arduíno;
+ - sensor ultrassônico de distância;
+ - motor de passo;
+ - ULN2003APG Darlington Array;
+ - Fonte de energia (5V/3A).
 
 ### Funcionamento
-O sensor ultrassônico, acoplado ao eixo do motor, será rotacionado em 360º, de modo que, a cada passo (estima-se um passo de aproximadamente 0,088º), será efetuada medições de distância. Dadas a medição média e o ângulo para cada passo, a aplicação será responsável por conectar todos os pontos obtidos e gerar uma imagem com a planta obtida. Por fim, será gerado um relatório sobre a **área** e o **perímetro** estimados do ambiente.
+O sensor ultrassônico, acoplado ao eixo do motor, será rotacionado em 360º, de modo que, a cada passo (estima-se um passo de aproximadamente 1,8º), serão efetuadas medições de distância. Dadas a medição média e o ângulo para cada passo, a aplicação será responsável por conectar todos os pontos obtidos e gerar uma imagem com a planta obtida. Por fim, será gerado um relatório sobre a **área** e o **perímetro** estimados do ambiente.
+
+### TODO List
+[X] Adicionar a API do sensor ultrassônico
+[X] Adicionar a API do motor de passos
+[] Salvar medidas em um arquivo .txt
+[] Adicionar um botão para medir (e pausar) e implementar sua interrupção
+[] Testar o sensor ultrassônico em cantos
+[] Melhorar a estrutura física
+[] Identificar a figura geométrica
+[] Calcular a área
+[] Gerar arquivo de saída final
+
+## Grupo
+Felipe Khoji Myose
+João Victor Pacheco
+Nicole Zafalon Kovacs
+Vinicius Ito Nagura
--- Trenuino/Trenuino.ino
+++ Trenuino/Trenuino.ino
@@ -1,63 +1,114 @@
-// Trenuino
-// Projeto Desenvolvido por:
-// Felipe Khoji Myose
-// Nicole Zafalon Kovacs
-// Vinicius Ito Nagura
+/* Trenuino
+ * Projeto Desenvolvido por:
+ *  Felipe Khoji Myose
+ *  João Victor Pacheco
+ *  Nicole Zafalon Kovacs
+ *  Vinicius Ito Nagura
+ *  
+ * Referências:
+ *  Motor:
+ *    https://www.arduino.cc/en/Reference/Stepper
+ *    https://www.arduino.cc/en/Tutorial/StepperOneRevolution
+ *  Sensor ultrassônico:
+ *    http://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/http://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
+*/
 
-// Registro para medicoes
+#include <Stepper.h>
+
+#define nMedidas 5
+#define stepsPerRevolution 200
+
+/* Cabeçalho */
+void obterMedidas();
+int medirDistancia(int trigPin, int echoPin);
+int media(int distanciasTemporarias[]);
+
+/* Registro para medicoes */
 struct Medida{
   float grau;
   int distancia;
 };
 
-/ *
-  Declaracao das variaveis
-*/
+/* Declaracao das variaveis globais */
+Medida medidas[200];
 
-// Uma unica medida, por enquanto (ainda sem motor)
-Medida medida1;
-
-// Parametros para API do sensor
+/* Parametros para API do sensor */
 long duration;
 int distance;
 
-// Pinos
+/* Pinos */
 const int trigPin = 13;
 const int echoPin = 12;
-   
-   
+const int m1      = 8;
+const int m2      = 9;
+const int m3      = 10;
+const int m4      = 11;
+
+/* Inicializar a biblioteca do motor de passo */
+Stepper myStepper(stepsPerRevolution, m1, m2, m3, m4);
+
 void setup() {
   pinMode(trigPin, OUTPUT); // Saida da onda
   pinMode(echoPin, INPUT);  // Retorno da onda
-  // Serial para acompanhar as medicoes (sera excluida posteriormente)
-  Serial.begin(9600);
+
+  myStepper.setSpeed(10);   // Setar a velocidade do motor para 10 rpm 
+  
+  Serial.begin(9600);       // Serial para acompanhar as medicoes (sera excluida posteriormente)
 }
 
-// API para medicao da distancia
-// retirada de:
-// http://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/http://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/
-int medeDistancia(int trigPin, int echoPin){
-    // Clears the trigPin
+void loop() {
+  obterMedidas();
+  delay(5000);
+}
+
+int media(int distanciasTemporarias[]) {
+  int sum = 0;
+  for(int j = 0; j < nMedidas; j++) {
+    sum += distanciasTemporarias[j];
+  }
+  return (sum/nMedidas);
+}
+
+void obterMedidas() {
+  int distanciasTemporarias[nMedidas];
+  
+  /* Girar o motor de passo e medir a distância */
+  for(int i = 0; i < 200; i++) {
+    myStepper.step(1);
+
+    delay(50);
+    
+    for(int j = 0; j < nMedidas; j++) {
+      distanciasTemporarias[j] = medirDistancia(trigPin, echoPin);
+      
+      // Printa a medicao
+      Serial.print("Distance: ");
+      Serial.println(distanciasTemporarias[j]);
+      
+      delay(50);
+    }
+    
+    medidas[i].distancia = media(distanciasTemporarias);
+    medidas[i].grau = (i + 1) * 1.8;
+  }
+  
+  /* Voltar o motor para a posição inicial */
+  myStepper.step(-200);
+}
+
+int medirDistancia(int trigPin, int echoPin){
+    // Reseta o trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
+    // Seta o trigPin para HIGH por 10 micro-segundos
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
+    // Lê o echoPin, retorna o travel time da onda sonora em micro-segundos
     duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
+    // Calcula a distância
     distance= duration*0.034/2;
-    // Prints the distance on the Serial Monitor
+
     return distance;
 }
 
-
-void loop() {
-  // Por enquanto, esta efetuando apenas uma medicao
-  medida1.distancia = medeDistancia(trigPin, echoPin);
-  // Printa a medicao
-  Serial.print("Distance: ");
-  Serial.println(medida1.distancia);
-  delay(500);
-}
